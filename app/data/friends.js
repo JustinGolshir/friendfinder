ac5514792c9e71c7f3bd176e50b9d3fadb54d9d9
justin <justin@tcadmins-MacBook-Pro-59.local>
2018-04-20 15:03:52 -0400

    bug fixes

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friends = [
+    {
+        name: "Gisele Bündchen",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Gisele_Bundchen2.jpg/800px-Gisele_Bundchen2.jpg",
+        scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+    {
+        name: "Adriana Lima",
+        photo: "https://imagesvc.timeincuk.net/v3/keystone/image?w=635&url=http://ksassets.timeincuk.net/wp/uploads/sites/46/2016/05/Best-Beauty-Cannes-Film-Festival-2016-Adriana-Lima--1280x1920.jpg&q=82",
+        scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+    },
+    {
+        name: "Gigi Hadid",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Gigi_Hadid_2016.jpg/800px-Gigi_Hadid_2016.jpg",
+        scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        name: "Emily Ratajkowski",
+        photo: "https://pbs.twimg.com/profile_images/886588839816343554/yZamOU6p_400x400.jpg",
+        scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+    },
+  ];
+  
+  // Export the array to make it accessible to other files using require.
+  module.exports = friends;
